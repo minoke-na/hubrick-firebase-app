3ad4eee2a82112d6289f59991768e47848e12d9b
Romina Felahi <felahi.romina@vimn.com>
2017-07-03 16:28:11 +0200

    Initial Commit

---
+++ hubrick-firebaseApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hubrick-firebaseApp
+//
+//  Created by Romina Felahi on 03.07.17.
+//  Copyright © 2017 --AnonymousChallenge--. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
